ec26aa56ca80145af56b755bc3f3af88b979ddb9
Jean Pierre Dudey <jeandudey@hotmail.com>
2016-07-08 21:55:51 -0400

    feat(fern, log): Add logging to brwm.
    Add fern and log crates to dependencies and setup a basic logger functionality.
    
    TODO: Support loggin to file.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ extern crate libc;
 #[macro_use]
 extern crate lazy_static;
 
+#[macro_use]
+extern crate log;
+extern crate fern;
+extern crate time;
+
 mod screen;
 mod display;
 
@@ -13,21 +18,36 @@ use window_manager::WindowManager;
 use std::error::Error;
 
 fn main() {
+    let logger_config = fern::DispatchConfig {
+        format: Box::new(|msg: &str, level: &log::LogLevel, _location: &log::LogLocation| {
+            format!("[{}][{}] {}", time::now().strftime("%Y-%m-%d][%H:%M:%S").unwrap(), level, msg)
+        }),
+        output: vec![fern::OutputConfig::stdout()],
+        level: log::LogLevelFilter::Trace,
+    };
+
+    if let Err(e) = fern::init_global_logger(logger_config, log::LogLevelFilter::Trace) {
+        println!("Failed to initialize global logger: {}", e);
+        return;
+    }
+
+    info!("Creating WindowManager.");
     let mut wm = match WindowManager::new() {
         Some(wm) => wm,
         None => {
-            println!("brwm: Cannot create window manager");
+            error!("Cannot create WindowManager");
             return;
         }
     };
 
+    info!("Running WindowManager.");
     match wm.run() {
         Ok(_) => {
-            println!("Success!");
+            info!("Exiting...");
             return;
         },
         Err(e) => {
-            println!("Error: {}", e.description());
+            error!("Error: {}", e.description());
             return;
         }
     }
